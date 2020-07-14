dcd1e0fee17d4f64e0568aea5df4786f1767288b
Rafay Aleem <rafay.aleem@pointclickcare.com>
2020-07-14 14:28:24 -0400

    Add helm chart for chapter 2

---
+++ dags/kubernetes_pod_example.py
@@ -0,0 +1,50 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(0),
+    'catchup': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'example_kubernetes_dag',
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=30),
+    max_active_runs=1,
+    concurrency=10
+)
+
+# Generate 5 tasks
+tasks = ["task{}".format('One') for i in range(1, 2)]
+example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
+
+org_dags = []
+for task in tasks:
+
+    bash_command = 'echo HELLO'
+
+    org_node = KubernetesPodOperator(
+        namespace='default',
+        image="python",
+        cmds=["python", "-c"],
+        arguments=["print('HELLO')"],
+        labels={"foo": "bar"},
+        image_pull_policy="Always",
+        name=task,
+        task_id=task,
+        get_logs=True,
+        dag=dag
+    )
+
+    org_node.set_downstream(example_dag_complete_node)
